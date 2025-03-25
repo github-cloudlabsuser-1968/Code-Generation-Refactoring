25a22e5647b8ce9cfef4bc5a5fec902b252cf9d3
github-cloudlabsuser-1968 <github_cloudlabsuser_1968@cloudlabsaiuser.com>
2025-03-25 15:16:52 +0000

    Reparar

--- card_draw.py
+++ card_draw.py
@@ -4,12 +4,12 @@
 import itertools, random
 
 # make a deck of cards
-deck = list(itertools.product(range(1,14),['Spade','Heart','Diamond','Club'])
+deck = list(itertools.product(range(1, 14), ['Spade', 'Heart', 'Diamond', 'Club']))  # Fixed missing parenthesis
 
 # shuffle the cards
 random.shuffle(deck)
 
 # draw five cards
 print("You got:")
-for i in range(5)
-   print(deck[i][0], "of", deck[i][1]
+for i in range(5):  # Fixed missing colon
+    print(deck[i][0], "of", deck[i][1])  # Fixed missing parenthesis
